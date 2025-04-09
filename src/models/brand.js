bc00a9a642c714d822b742d13d84c435d45b4754
nefims <nefims@gmail.com>
2025-04-09 19:33:24 -0300

    Brand and Warehouse models

---
+++ src/models/brand.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const brandSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Brand', brandSchema);
