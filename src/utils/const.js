d88a2ef8f16a981b85907fd0a5b496044ba52f6f
Vermont Garcia <vermontgarcia@gmail.com>
2025-04-09 17:20:08 -0600

    Swagger Documentation configuration

---
+++ src/utils/const.js
@@ -0,0 +1,5 @@
+const apiDocDescription = `The Warehouse Management API is designed to efficiently manage multiple warehouses, each containing various product categories such as groceries, tools, plants, and clothing. This API facilitates warehouse operations, inventory tracking, and product management while ensuring secure user authentication and role-based access control.`;
+
+module.exports = {
+  apiDocDescription,
+};
