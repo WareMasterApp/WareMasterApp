eb2b956fadd8f7293a998489ed005a7adefa110f
vermontgarcia <40477541+vermontgarcia@users.noreply.github.com>
2025-04-15 17:16:21 -0600

    Google OAuth (#23)
    * OAuth for Google
    
    * Landing Page form updated for Google Login
    
    ---------
    
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>

--- src/helpers/pasportStrategy.js
+++ src/helpers/pasportStrategy.js
@@ -1,9 +1,13 @@
 const passportStrategy = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const {
   GH_CLIENT_ID,
   GH_CLIENT_SECRET,
   GH_CALLBACK_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL,
 } = require('../utils/const.env');
 
 passportStrategy.use(
@@ -15,7 +19,23 @@ passportStrategy.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       // TODO: Add a user to the database
-      // User.create(profile);
+      // For simplicity, we'll just return the profile here
+      return done(null, profile);
+    }
+  )
+);
+
+passportStrategy.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: GOOGLE_CALLBACK_URL,
+      passReqToCallback: true, // Allow access to req object in callback
+    },
+    (req, accessToken, refreshToken, profile, done) => {
+      // TODO: Add a user to the database
+      // For simplicity, we'll just return the profile here
       return done(null, profile);
     }
   )
