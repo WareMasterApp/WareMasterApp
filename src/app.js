bd9f62ba53ac960ca6f8271cd431ce2831c380ca
vermontgarcia <40477541+vermontgarcia@users.noreply.github.com>
2025-04-15 10:31:17 -0600

    Session configuration (#22)
    Passport Configuration
    OAuth for GitHub
    
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
 const cors = require('cors');
 const express = require('express');
 const logger = require('morgan');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const swaggerUI = require('swagger-ui-express');
-const { swaggerSpec, swaggerUiOptions } = require('./docs/apiDoc');
+const passportStrategy = require('./helpers/pasportStrategy');
+const authRouter = require('./routes/authRouter');
 const accountRouter = require('./routes/accountRouter');
 const brandRouter = require('./routes/brandRouter');
+const { swaggerSpec, swaggerUiOptions } = require('./docs/apiDoc');
+const { SECRET } = require('./utils/const.env');
 
 const app = express();
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(session({ secret: SECRET, resave: true, saveUninitialized: true }));
+app.use(passportStrategy.initialize());
+app.use(passportStrategy.session());
 
 /**
  * Landing Page Static Routes
  */
 app.use(express.static('src/public'));
+
+/**
+ * Auth Routes
+ */
+app.use('/auth', authRouter);
+
+/**
+ * API Routes
+ */
+app.use('/api/v1/account', accountRouter);
+app.use('/api/v1/brand', brandRouter);
 
 /**
  * Swagger Documentation
@@ -25,11 +43,5 @@ app.use(
   swaggerUI.serve,
   swaggerUI.setup(swaggerSpec, swaggerUiOptions)
 );
-
-/**
- * API Routes
- */
-app.use('/api/v1/account', accountRouter);
-app.use('/api/v1/brand', brandRouter);
 
 module.exports = app;
---
+++ src/controllers/authController.js
@@ -0,0 +1,19 @@
+const gitHubLogin = (req, res) => {
+  console.log('USER: ', req.user);
+  req.session.user = req.user;
+  res.redirect('/api-docs');
+};
+
+const logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+};
+
+module.exports = {
+  gitHubLogin,
+  logout,
+};
